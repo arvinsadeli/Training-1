1d73a5a936080256c26face710a6c078f3b9e336
Arvin Sadeli <arvinsadeli1@gmail.com>
2024-02-20 17:06:43 +0700

    Wall Jump, movement, anim

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -4,70 +4,127 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    private bool isJumping;
     public MovementClass movement;
     Rigidbody2D rb;
     Animator anim;
+    SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] LayerMask wallLayer;
+    RaycastHit2D raycastHit2D;
+    float horizontalInput;
+    float wallJumpCooldown;
+    float initialGravity;
+
     // Start is called before the first frame update
 
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        initialGravity = rb.gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement(movement.velocity);
-        Jump(movement.jumpForce);
+        horizontalInput = Input.GetAxis("Horizontal");
+        Movement(movement.speed);
+        WallJump();
     }
 
     public void Movement(float velocity)
     {
-        float input = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(input * velocity, rb.velocity.y);
-        if(input < -.01f)
+        //rb.velocity = new Vector2(input * velocity, rb.velocity.y);
+        if (horizontalInput < -.01f) //flip left
         {
-            Debug.Log(input + "Going left");
             transform.localScale = new Vector3(-1 * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
-        if (input > .01f)
+        if (horizontalInput > .01f) //flip right
         {
-            Debug.Log(input + "Going Right");
             transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
 
-        anim.SetBool("isRunning", rb.velocity.x != 0);
+        anim.SetBool("isRunning", rb.velocity.x > 0.1f || rb.velocity.x < -.1f);
     }
 
-    public void Jump(float jumpForce)
+    public void WallJump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            isJumping = true;
-        }
-    }
 
-    IEnumerator CheckIfLanded()
-    {
-        if(rb.velocity.y <= 0)
+        if(wallJumpCooldown > .2f)
         {
-            isJumping = false;
+            rb.velocity = new Vector2(horizontalInput * movement.speed, rb.velocity.y);
+
+            if(isOnWall() && !isGrounded())
+            {
+                rb.gravityScale = 0f;
+                rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                rb.gravityScale = initialGravity;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                NormalJump(movement.jumpForce);
+            }
         }
         else
         {
-            yield return new WaitForSeconds(.5f);
-            StartCoroutine(CheckIfLanded());
+            wallJumpCooldown += Time.deltaTime;
         }
-        Debug.Log("Velocity = " + rb.velocity.y);
+    }
+
+    public void NormalJump(float jumpForce)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(isGrounded()) //if grounded
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                anim.SetTrigger("jumpTrigger");
+            }
+            else
+            {
+                if(isOnWall()) //if not grounded and stik to the wall
+                {
+                    if(horizontalInput == 0)
+                    {
+                        wallJumpCooldown = 0f;
+                        rb.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * movement.speed, 0f);
+                    }
+                    else
+                    {
+                        wallJumpCooldown = 0f;
+                        rb.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * movement.speed, movement.speed * 2f);
+                    }
+                    //mathf (if f > 0 .. then it will be 1.. f <= 0 then it will be -1)
+                }
+            }
+        }
+
+        anim.SetBool("isGrounded", isGrounded());
+    }
+
+    private bool isGrounded()
+    {
+        raycastHit2D = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, .1f, groundLayer);
+        return raycastHit2D.collider != null;
+    }
+
+    private bool isOnWall()
+    {
+        raycastHit2D = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, new Vector2(transform.localScale.x, 0), .1f, wallLayer);
+        return raycastHit2D.collider != null;
     }
 
     [System.Serializable]
     public class MovementClass
     {
-        public float jumpForce = 3f;
-        public float velocity = 5f;
+        public float speed = 5f;
+        public float jumpForce = 8f;
     }
 }
